60d0c6fc3c1db1e38e5e937b15d96750e828664d
yjmini <74178991+yjmini@users.noreply.github.com>
2026-01-21 01:05:10 +0900

    level6_solutions

---
+++ Level_6/6_02.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main602() {
+	char a, b, c;
+	cin >> a >> b >> c;
+
+	cout << (int)a << endl << (int)b << endl << (int)c << endl;
+
+	return 0;
+}
---
+++ Level_6/6_03.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main603() {
+	int a;
+	cin >> a;
+
+	for (int i = 0; i < a; i++) {
+		for (int j = 1; j <= 5; j++) {
+			cout << j << " ";
+		}
+		cout << endl;
+	}
+
+	return 0;
+}
