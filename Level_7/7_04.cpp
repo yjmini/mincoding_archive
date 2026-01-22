05a08a9c9d00f25c5ac6ef595900d9b76c012d22
yjmini <74178991+yjmini@users.noreply.github.com>
2026-01-23 01:00:50 +0900

    level7_solutions

---
+++ Level_7/7_04.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+int main704() {
+	int arr[5];
+	int cnt = 0;
+	for (int i = 0; i < 5; i++) {
+		cin >> arr[i];
+		if (arr[i] >= 3 && arr[i] <= 7) {
+			cnt++;
+		}
+	}
+
+	cout << cnt;
+
+	return 0;
+}
---
+++ Level_7/7_05.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+int main705() {
+	int n;
+	cin >> n;
+
+	if (n >= 80) {
+		cout << "수";
+	}
+	else if (n >= 70) {
+		cout << "우";
+	}
+	else if (n >= 60) {
+		cout << "미";
+	}
+	else {
+		cout << "재시도";
+	}
+
+	return 0;
+}
