39d7ede1aa2c7db77c55354db6d3294d6c788a4b
yjmini <74178991+yjmini@users.noreply.github.com>
2026-02-04 23:16:43 +0900

    level24_solutions

---
+++ Level_24/24_01.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main2401()
+{
+	string str;
+	cin >> str;
+
+	cout << str << endl;
+	
+	for (int i = str.size()-1; i >= 0; i--)
+	{
+		cout << str[i];
+	}
+
+	return 0;
+}
