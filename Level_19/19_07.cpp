363bfccc5696c3691b97801aeba503b3e0ac8f3e
yjmini <74178991+yjmini@users.noreply.github.com>
2026-02-05 21:18:05 +0900

    level19_solutions

---
+++ Level_19/19_07.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+using namespace std;
+
+int main1907() {
+	int input[4][2];
+	for (int i = 0; i < 4; i++)
+	{
+		for (int j = 0; j < 2; j++)
+		{
+			cin >> input[i][j];
+		}
+	}
+
+	int vect[4][3] = { 0 };
+
+	for (int i = 0; i < 4; i++)
+	{
+		vect[input[i][0]][input[i][1]] = 5;
+	}
+
+	for (int i = 0; i < 4; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			cout << vect[i][j] << " ";
+		}
+		cout << endl;
+	}
+
+	return 0;
+}
---
+++ Level_19/19_08.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+using namespace std;
+
+int image[4][4];
+
+int rectSum(int y, int x)
+{
+	int sum = 0;
+
+	for (int i = 0; i < 2; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			sum += image[y + i][x + j];
+		}
+	}
+	return sum;
+}
+
+int main1908() {
+	int maxSum = 0;
+	int y, x;
+
+	for (int i = 0; i < 4; i++)
+	{
+		for (int j = 0; j < 4; j++)
+		{
+			cin >> image[i][j];
+		}
+	}
+
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 2; j++)
+		{
+			if (maxSum < rectSum(i, j))
+			{
+				maxSum = rectSum(i, j);
+				y = i;
+				x = j;
+			}
+		}
+	}
+
+	cout << "(" << y << "," << x << ")";
+
+	return 0;
+}
