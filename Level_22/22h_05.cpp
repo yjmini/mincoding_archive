04cf45a10ddcaafb8a2401f4d512662a6029e13f
yjmini <74178991+yjmini@users.noreply.github.com>
2026-02-08 14:27:54 +0900

    level22_solutions

---
+++ Level_22/22h_05.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int main22005() {
+	int map[3][6] = {
+		3,5,4,2,2,3,
+		1,3,3,3,4,2,
+		5,4,4,2,3,5
+	};
+	char price[5] = { 'T','P','G','K','C' };
+
+	char ch;
+	int n;
+	cin >> ch >> n;
+
+	int k = map[ch - 65][n - 1];
+	cout << price[k - 1];
+
+	return 0;
+}
---
+++ Level_22/22h_06.cpp
@@ -0,0 +1,32 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+using namespace std;
+
+
+int main22006() {
+	char str[4][11];
+	for (int i = 0; i < 4; i++)
+	{
+		cin >> str[i];
+	}
+
+    for (int i = 0; i < 4; i++) {
+        for (int j = i + 1; j < 4; j++) {
+            if (strlen(str[i]) > strlen(str[j])) {
+                char temp[11];
+                strcpy(temp, str[i]); 
+                strcpy(str[i], str[j]); 
+                strcpy(str[j], temp); 
+            }
+        }
+    }
+
+    for (int i = 0; i < 4; i++)
+    {
+        cout << str[i] << endl;
+    }
+
+	return 0;
+}
