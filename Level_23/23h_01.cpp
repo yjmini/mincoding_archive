4c058fcbc833f603e4463d64a01ea0b184ebcdbd
yjmini <74178991+yjmini@users.noreply.github.com>
2026-02-08 15:40:15 +0900

    level23_solutions

---
+++ Level_23/23h_01.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+using namespace std;
+
+int arrs[5] = { 3,5,1,9,7 };
+char rl[4];
+int results[5];
+
+void rot(int lev)
+{
+	if (lev == 4)
+	{
+		for (int i = 0; i < 5; i++)
+		{
+			// cout << arrs[i] << " ";
+			return;
+		}
+	}
+
+	if (rl[lev] == 'R')
+	{
+		int temp;
+		temp = arrs[4];
+		for (int j = 4; j > 0; j--)
+		{
+			arrs[j] = arrs[j - 1];
+		}
+		arrs[0] = temp;
+	}
+	else if (rl[lev] == 'L')
+	{
+		int temp;
+		temp = arrs[0];
+		for (int j = 0; j < 4; j++)
+		{
+			arrs[j] = arrs[j + 1];
+		}
+		arrs[4] = temp;
+	}
+	rot(lev + 1);
+
+}
+
+int main23001() {
+	for (int i = 0; i < 4; i++)
+	{
+		cin >> rl[i];
+	}
+
+	rot(0);
+
+	for (int i = 0; i < 5; i++)
+	{
+		cout << arrs[i] << " ";
+	}
+
+	return 0;
+}
---
+++ Level_23/23h_02.cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>
+using namespace std;
+
+char murder[3][5];
+
+int isSafe()
+{
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 4; j++)
+		{
+			for (int k = j + 1; k < 4; k++)
+			{
+				if (murder[i][j] == '#' && murder[i][k] == '#')
+				{
+					return 1;
+				}
+
+			}
+		}
+	}
+
+	for (int i = 0; i < 4; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			for (int k = j+1; k < 3; k++)
+			{
+				if (murder[j][i] == '#' && murder[k][i] == '#')
+				{
+					return 1;
+				}
+
+			}
+		}
+	}
+	return 0;
+}
+
+int main23002() {
+	for (int i = 0; i < 3; i++)
+	{
+		int y;
+		int x;
+		cin >> y >> x;
+		murder[y][x] = '#';
+	}
+
+	if (isSafe() == 1)
+	{
+		cout << "위험";
+	}
+	else
+	{
+		cout << "안전";
+	}
+
+	return 0;
+}
