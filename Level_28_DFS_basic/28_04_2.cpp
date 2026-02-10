f7bfe22240fc33e781644e979594f32ec877a123
yjmini <74178991+yjmini@users.noreply.github.com>
2026-02-10 17:56:05 +0900

    level28_solutions

---
+++ Level_28_DFS_basic/28_04_2.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+using namespace std;
+
+int n;
+int arr[100][100];
+
+void dfs(int node)
+{
+	cout << node << " ";
+
+	for (int i = 0; i < n; i++)
+	{
+		if (arr[node][i] == 0)
+			continue;
+		dfs(i);
+	}
+}
+
+int main()
+{
+	cin >> n;
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			cin >> arr[i][j];
+		}
+	}
+
+	dfs(0);
+
+	return 0;
+}
