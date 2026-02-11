35e0c97adb3faddae3d9a945f698ae4f8bfd0177
yjmini <74178991+yjmini@users.noreply.github.com>
2026-02-11 11:24:04 +0900

    level29_solutions

---
+++ Level_29_DFS/29_01.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <vector>
+#include <cstring>
+#include <string>
+using namespace std;
+
+int N;
+int arr[101][101];
+
+void dfs(int now)
+{
+	cout << now << " ";
+	for (int i = 0; i < N; i++)
+	{
+		if (arr[now][i] == 0)
+			continue;
+
+		dfs(i);
+	}
+}
+
+int main()
+{
+	cin >> N;
+
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < N; j++)
+		{
+			cin >> arr[i][j];
+		}
+	}
+
+	dfs(0);
+
+	return 0;
+}
---
+++ Level_29_DFS/29_02.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int map[7][7] = {
+	0,0,0,0,0,0,0,
+	0,0,0,1,0,1,1,
+	0,1,0,0,1,0,0,
+	0,0,0,0,0,1,0,
+	0,1,0,0,0,0,0,
+	0,1,0,0,0,0,0,
+	0,0,0,0,0,0,0
+};
+int st, en;
+int minCost = 21e8;
+int visited[7];
+
+void dfs(int now, int cost)
+{
+	if (now == en)
+	{
+		minCost = min(minCost, cost);
+		return;
+	}
+
+	for (int i = 0; i < 7; i++)
+	{
+		if (map[now][i] == 0)
+			continue;
+		if (visited[i] == 1)
+			continue;
+		visited[now] = 1;
+		dfs(i, cost + 1);
+		visited[now] = 0;
+	}
+}
+
+int main() 
+{
+	cin >> st >> en;
+
+	dfs(st, 0);
+
+	if (minCost == 21e8)
+	{
+		cout << 0;
+	}
+	else
+	{
+		cout << minCost;
+	}
+
+	return 0;
+}
