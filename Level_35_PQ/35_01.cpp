1ec3df6b2d1a157ebce4b28148e782a3d56f3523
yjmini <74178991+yjmini@users.noreply.github.com>
2026-02-28 01:31:32 +0900

    level35_solutions

---
+++ Level_35_PQ/35_01.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <string>
+using namespace std;
+
+int main()
+{
+	priority_queue<int> pq;
+
+	string str;
+	cin >> str;
+
+	for (int i = 0; i < str.length(); i++)
+	{
+		pq.push(str[i]);
+	}
+
+	while (!pq.empty())
+	{
+		char now = pq.top();
+		pq.pop();
+
+		cout << now;
+	}
+
+	return 0;
+}
---
+++ Level_35_PQ/35_02.cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+using namespace std;
+
+struct Edge
+{
+	int a;
+	int b;
+	int cost;
+};
+
+struct cmp
+{
+	bool operator() (Edge a, Edge b)
+	{
+		return a.cost < b.cost;
+	}
+};
+
+int main()
+{
+	int N;
+	cin >> N;
+
+	vector<vector<int>> map(N, vector<int>(N));
+
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < N; j++)
+		{
+			cin >> map[i][j];
+		}
+	}
+
+	priority_queue<Edge, vector<Edge>, cmp> pq;
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < N; j++)
+		{
+			if (map[i][j] > 0)
+			{
+				pq.push({ i, j, map[i][j] });
+			}
+		}
+	}
+	int cnt = 0;
+	while (!pq.empty())
+	{
+		Edge now = pq.top();
+		pq.pop();
+
+		cout << (char)(now.a + 65) << "-" << (char)(now.b + 65) << " " << now.cost << '\n';
+		cnt++;
+		if (cnt == 3) break;
+	}
+
+	return 0;
+}
